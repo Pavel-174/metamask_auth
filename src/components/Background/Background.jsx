83002186239f7decb90d7f8c1573f1277011b38a
Pavel Alekseev <pavelbooking.com@yandex.ru>
2024-04-15 19:46:13 +0500

    background

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,14 @@
+import './Background.scss';
+
+function Background() {
+  return (
+    <div className='background'>
+        <div className='elipse elipse1'/>
+        <div className='elipse elipse2'/>
+        <div className='elipse elipse3'/>
+        <div className='elipse elipse4'/>
+    </div>
+  );
+}
+
+export default Background;
