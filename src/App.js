052434730235f4a6f4e73ee92c0c860b4a2dea7a
Pavel Alekseev <pavelbooking.com@yandex.ru>
2024-04-16 23:22:13 +0500

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Background from './components/Background/Background';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
@@ -9,9 +10,7 @@ function App() {
       <Background />
       <Header />
       <Main />
-      <footer>
-
-      </footer>
+      <Footer />
     </div>
   );
 }
