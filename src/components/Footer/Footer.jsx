55f59d8edc33e2a107dc0ea2d620cb77a5c5432c
Pavel Alekseev <pavelbooking.com@yandex.ru>
2024-04-17 00:01:00 +0500

    footer desktop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,58 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './Footer.scss';
+import logo from '../../img/LOGO.svg'
+import facebook from '../../img/facebook.svg'
+import instagram from '../../img/instagram.svg'
+import twitter from '../../img/twiter.svg'
+import youtube from '../../img/youtube.svg'
 
 function Footer() {
     return (
         <footer className="footer">
-
+            <div className='footer__links'>
+                <a className='footer__link' href='#'>Privacy Policy</a>
+                <a className='footer__link' href='#'>Terms & Conditions</a>
+                <a className='footer__link' href='#'>Cookie Policy</a>
+            </div>
+            <div className='footer__main'>
+                <img 
+                    className='footer__logo'
+                    src={logo}
+                    alt='logo'
+                />
+                <span className='footer__copyright'>©2022 All rights reserved. Powered by Atla</span>
+            </div>
+            <div className='footer__social'>
+                <a className='footer__link' href='#'>
+                    <img 
+                        className='footer__logo'
+                        src={facebook}
+                        alt='facebook'
+                    />
+                </a>
+                <a className='footer__link' href='#'>
+                    <img 
+                        className='footer__logo'
+                        src={twitter}
+                        alt='twitter'
+                    />
+                </a>
+                <a className='footer__link' href='#'>
+                    <img 
+                        className='footer__logo'
+                        src={youtube}
+                        alt='youtube'
+                    />
+                </a>
+                <a className='footer__link' href='#'>
+                    <img 
+                        className='footer__logo'
+                        src={instagram}
+                        alt='instagram'
+                    />
+                </a>
+            </div>
         </footer>
     );
 }
